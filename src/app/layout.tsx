2e9a7a490d08db1b3daa0545d57b77e43724f44e
CryptoTotalWar <pablo@dltpartners.io>
2023-11-27 18:31:45 -0500

    fix header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo app",
+  title: "Info Merge",
 };
 
 export default function RootLayout({
