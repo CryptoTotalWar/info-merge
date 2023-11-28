82de2f21212a4a2af6036cc9cf519a324354e0d5
CryptoTotalWar <pablo@dltpartners.io>
2023-11-27 22:37:37 -0500

    fixed error with page not rendering, removed autogenerated app/globals.css file, edited a bunch of other stuf for config, etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"; // Rename Inter to FontSans for consistency.
 import "./globals.css";
+import { cn } from "@/lib/utils"; // Ensure you have the cn function in your utils or create one.
 
-const inter = Inter({ subsets: ["latin"] });
+// Instantiate the FontSans with the subsets and a CSS variable.
+// export const fontSans = FontSans({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
 
 export const metadata: Metadata = {
   title: "Info Merge",
   description:
-    "Your Ultimate Millennial News Hub with human curated media feeds. You can say bye to Drudge Report for good now! ",
+    "Your Ultimate Millennial News Hub with human curated media feeds. You can say bye to Drudge Report for good now!",
 };
 
 export default function RootLayout({
@@ -17,8 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body
-        className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}
+        // className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}
+        // className={cn(
+        //   "min-h-screen bg-background font-sans antialiased",
+        //   fontSans.variable // Use the CSS variable for the font family.
+        // )}
       >
         {children}
       </body>
