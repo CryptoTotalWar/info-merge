38ce983a6b4d16758714f15762e014fff24a83d0
CryptoTotalWar <pablo@dltpartners.io>
2023-11-29 00:50:44 -0500

    placeholder for our form

--- src/app/admin/create-headline/page.tsx
+++ src/app/admin/create-headline/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
+import CreateHeadlineForm from "@/components/CreateHeadlineForm";
 
 export default function CreateHeadlinePage() {
   return (
     <>
       <h1 className="text-2xl">Create Headline Page</h1>
+      <CreateHeadlineForm />
+
       {/* Additional content for the Create Headline Page */}
       {/* // </AdminLayout> */}
     </>
--- src/components/CreateHeadlineForm.tsx
+++ src/components/CreateHeadlineForm.tsx
@@ -1,178 +1,66 @@
 "use client";
-import * as React from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
-import { Slot } from "@radix-ui/react-slot";
-import {
-  Controller,
-  ControllerProps,
-  FieldPath,
-  FieldValues,
-  FormProvider,
-  useFormContext,
-} from "react-hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-import { cn } from "@/lib/utils";
+import { Button } from "@/components/form-components/button";
+import { Checkbox } from "@/components/form-components/checkbox";
+import { Input } from "@/components/form-components/input";
 import { Label } from "@/components/form-components/label";
+import {
+  RadioGroup,
+  RadioGroupItem,
+} from "@/components/form-components/radio-group";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from "@/components/form-components/select";
+import { Textarea } from "@/components/form-components/textarea";
 
-const Form = FormProvider;
+export default function CreateHeadlineForm() {
+  const { control, handleSubmit } = useForm(); // We will define the form schema and validation rules later
 
-type FormFieldContextValue<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> = {
-  name: TName;
-};
-
-const FormFieldContext = React.createContext<FormFieldContextValue>(
-  {} as FormFieldContextValue
-);
-
-const FormField = <
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
->({
-  ...props
-}: ControllerProps<TFieldValues, TName>) => {
-  return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
-      <Controller {...props} />
-    </FormFieldContext.Provider>
-  );
-};
-
-const useFormField = () => {
-  const fieldContext = React.useContext(FormFieldContext);
-  const itemContext = React.useContext(FormItemContext);
-  const { getFieldState, formState } = useFormContext();
-
-  const fieldState = getFieldState(fieldContext.name, formState);
-
-  if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>");
-  }
-
-  const { id } = itemContext;
-
-  return {
-    id,
-    name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
-    ...fieldState,
-  };
-};
-
-type FormItemContextValue = {
-  id: string;
-};
-
-const FormItemContext = React.createContext<FormItemContextValue>(
-  {} as FormItemContextValue
-);
-
-const FormItem = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  const id = React.useId();
+  // Dummy category tags, to be replaced with dynamic data from your API
+  const categoryTags = ["News", "Updates", "Events"];
 
   return (
-    <FormItemContext.Provider value={{ id }}>
-      <div ref={ref} className={cn("space-y-2", className)} {...props} />
-    </FormItemContext.Provider>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <Label>Hyperlink</Label>
+      <Input {...control.register("hyperlink")} />
+
+      <Label>Category Tag</Label>
+      <Select>
+        <SelectTrigger>
+          <SelectValue placeholder="Select Category Tag" />
+        </SelectTrigger>
+        <SelectContent>
+          {categoryTags.map((tag) => (
+            <SelectItem key={tag} value={tag}>
+              {tag}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Label>New Category Tag</Label>
+      <Input {...control.register("newCategoryTag")} />
+
+      <Label>Headline Content</Label>
+      <Textarea {...control.register("headlineContent")} />
+
+      <Label>Is Breaking News?</Label>
+      <RadioGroup>
+        <RadioGroupItem value="yes" id="yes">
+          <Label htmlFor="yes">Yes</Label>
+        </RadioGroupItem>
+        <RadioGroupItem value="no" id="no">
+          <Label htmlFor="no">No</Label>
+        </RadioGroupItem>
+      </RadioGroup>
+
+      <Button type="submit">Submit</Button>
+    </form>
   );
-});
-FormItem.displayName = "FormItem";
-
-const FormLabel = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => {
-  const { error, formItemId } = useFormField();
-
-  return (
-    <Label
-      ref={ref}
-      className={cn(error && "text-destructive", className)}
-      htmlFor={formItemId}
-      {...props}
-    />
-  );
-});
-FormLabel.displayName = "FormLabel";
-
-const FormControl = React.forwardRef<
-  React.ElementRef<typeof Slot>,
-  React.ComponentPropsWithoutRef<typeof Slot>
->(({ ...props }, ref) => {
-  const { error, formItemId, formDescriptionId, formMessageId } =
-    useFormField();
-
-  return (
-    <Slot
-      ref={ref}
-      id={formItemId}
-      aria-describedby={
-        !error
-          ? `${formDescriptionId}`
-          : `${formDescriptionId} ${formMessageId}`
-      }
-      aria-invalid={!!error}
-      {...props}
-    />
-  );
-});
-FormControl.displayName = "FormControl";
-
-const FormDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
-  const { formDescriptionId } = useFormField();
-
-  return (
-    <p
-      ref={ref}
-      id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
-      {...props}
-    />
-  );
-});
-FormDescription.displayName = "FormDescription";
-
-const FormMessage = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, children, ...props }, ref) => {
-  const { error, formMessageId } = useFormField();
-  const body = error ? String(error?.message) : children;
-
-  if (!body) {
-    return null;
-  }
-
-  return (
-    <p
-      ref={ref}
-      id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
-      {...props}
-    >
-      {body}
-    </p>
-  );
-});
-FormMessage.displayName = "FormMessage";
-
-export {
-  useFormField,
-  Form,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormDescription,
-  FormMessage,
-  FormField,
-};
+}
