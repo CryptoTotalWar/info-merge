11bd504af22f98a020dae4fed68f9464c45e542e
CryptoTotalWar <pablo@dltpartners.io>
2023-11-29 04:23:56 -0500

    edit logo

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import AdminHeader from "@/components/AdminHeader";
+import Logo from "@/components/Logo"; // Import the Logo component
 
 export default function AdminLayout({
   children,
@@ -10,6 +11,9 @@ export default function AdminLayout({
 }) {
   return (
     <>
+      <div className="flex flex-col items-center justify-center bg-gray-800 py-6">
+        <Logo /> {/* Place the Logo component here */}
+      </div>
       <AdminHeader />
       <main className="container mx-auto py-6 px-4">{children}</main>
     </>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,15 @@
 // src/components/Logo.tsx
 "use client";
 import React from "react";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <h1 className="text-3xl font-semibold italic tracking-wider text-shadow-lg">
-      INFOMERGE
-    </h1>
+    <Link href="/">
+      <h1 className="text-3xl font-semibold italic tracking-wider text-shadow-lg hover:text-blue-500">
+        INFOMERGE
+      </h1>
+    </Link>
   );
 };
 
