7b4d518d2c7b8f505b34ded056b0ca57e62bbadc
CryptoTotalWar <pablo@dltpartners.io>
2023-11-30 00:07:54 -0500

    edit header

--- src/components/PublicHeader.tsx
+++ src/components/PublicHeader.tsx
@@ -6,10 +6,12 @@ import Logo from "./Logo"; // Update the import statement
 
 const PublicHeader = () => {
   return (
-    <header className="text-white py-4 px-6 mb-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Logo /> {/* Use the Logo component */}
-        <PublicNavigationMenu />
+    <header className="text-white py-4 px-6 mb-4 ">
+      <div className="container mx-auto flex flex-col items-center">
+        <nav className="self-end mt-2 space-x-4">
+          <PublicNavigationMenu />
+        </nav>
+        <Logo />
       </div>
     </header>
   );
