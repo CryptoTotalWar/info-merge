7ea5765896b2aa87403eb81c05b84de62e5f8d12
CryptoTotalWar <pablo@dltpartners.io>
2023-11-29 21:53:06 -0500

    get good bkg color

--- src/components/PublicHeader.tsx
+++ src/components/PublicHeader.tsx
@@ -6,7 +6,7 @@ import Logo from "./Logo"; // Update the import statement
 
 const PublicHeader = () => {
   return (
-    <header className="bg-gray-800 text-white py-4 px-6">
+    <header className="bg-slate-900 bg-opacity-60 text-white py-4 px-6">
       <div className="container mx-auto flex justify-between items-center">
         <Logo /> {/* Use the Logo component */}
         <PublicNavigationMenu />
