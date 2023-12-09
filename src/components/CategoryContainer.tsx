99508096fdee06db675e848c7ac7ade849dc6ff0
CryptoTotalWar <pablo@dltpartners.io>
2023-12-08 19:35:28 -0500

    styling fixes

--- src/components/CategoryContainer.tsx
+++ src/components/CategoryContainer.tsx
@@ -20,7 +20,7 @@ export function CategoryContainer({
       <h3 className="text-xl font-bold mb-4 uppercase text-center">
         {categoryTag}
       </h3>
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-col gap-2">
         {headlines.map((headline) => (
           <HeadlineCard key={headline.id} headline={headline} />
         ))}
