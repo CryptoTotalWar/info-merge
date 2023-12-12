874891ef17b35492f6d98859afcd9fc4e8f9588c
CryptoTotalWar <pablo@dltpartners.io>
2023-12-12 08:18:43 -0500

    refresh working except when using filter

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,12 +4,18 @@ import React from "react";
 import Link from "next/link";
 
 const Logo = () => {
+  const handleClick = (event: React.MouseEvent<HTMLHeadingElement>) => {
+    event.preventDefault();
+    window.location.href = "/";
+  };
+
   return (
-    <Link href="/">
-      <h1 className="transform skew-x-12 shadow-inner text-4xl py-2 px-10 font-semibold drop-shadow-2x1 italic tracking-wider bg-slate-900 opacity-90 text-shadow-lg shadow-slate-900 hover:text-slate-900">
-        INFO-MERGE
-      </h1>
-    </Link>
+    <h1
+      onClick={handleClick}
+      className="transform skew-x-12 shadow-inner text-4xl py-2 px-10 font-semibold drop-shadow-2x1 italic tracking-wider bg-slate-900 opacity-90 text-shadow-lg shadow-slate-900 hover:text-slate-900"
+    >
+      INFO-MERGE
+    </h1>
   );
 };
 
